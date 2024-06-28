6b4b22909df178eead82a8e28ad7e7ffddfb9c20
ErikaCZanin <erikaczbu@gmail.com>
2024-06-27 22:53:23 -0300

    Atualização

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-
   return (
     <div>
-      <div>Teste</div>
-      <div>Teste</div>
+      <div className='intro'>
+        <div className='introh1'>
+        <h1> Sistema solar</h1>
+        </div>
+          <p>teste</p>
+      </div>
+      <div className='content-container'>
+        <div className='container'>
+          <h1>Sol</h1>
+        </div>
+        <div className='container'>
+          <h1>Terra</h1>
+          <div>
+          <p>teste</p>
+          </div>
+        </div>
+        <div className='container'>
+          <h1>Marte</h1>
+          <p>teste</p>
+        </div>
+      </div>
     </div>
   )
 }
