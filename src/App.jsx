d6512bea34ab088ed9d9fedb36172bdecb3fe83a
ErikaCZanin <erikaczbu@gmail.com>
2024-06-27 23:29:59 -0300

    Atualizando css

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,17 @@ function App() {
   return (
     <div>
       <div className='intro'>
-        <div className='introh1'>
         <h1> Sistema solar</h1>
-        </div>
           <p>teste</p>
       </div>
       <div className='content-container'>
         <div className='container'>
           <h1>Sol</h1>
+          <p>teste</p>
         </div>
         <div className='container'>
           <h1>Terra</h1>
-          <div>
           <p>teste</p>
-          </div>
         </div>
         <div className='container'>
           <h1>Marte</h1>
